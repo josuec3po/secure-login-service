f852336ae4ab5a9a554bf17dab9acf83fa4b237c
Josué Franco Braga <josuefbraga@outlook.com>
2026-05-12 15:33:56 -0300

    feat/refactor: nova função de registro e alteração na lógica de login
    - Alterada lógica de login para funcionamento dinâmico ao invés de fixo
    - Criada função de registro
    - Adicionar .gitignore

--- login.py
+++ login.py
@@ -6,45 +6,93 @@ O UTF-8 é o padrão dominante de codificação de caracteres na web,
 mapeando caracteres Unicode para sequências de 1 a 4 bytes, 
 garantindo compatibilidade com ASCII.
 '''
-# DB =======================================================================================
+# CRIA DB SE NÃO EXISTIR =======================================================================================
 connection = sqlite3.connect("banco.db")
 cursor = connection.cursor()
 
-cursor.execute("""CREATE TABLE login_info (
+cursor.execute("""CREATE TABLE IF NOT EXISTS login_info (
                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                login TEXT NOT NULL,
                senha TEXT NOT NULL
                )""")
 connection.commit()
-# FUNÇOES ==================================================================================
-def bcrypt_hash(password:str):
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=12))
 
+# FUNÇOES ==================================================================================
+
+# CRITOGRAFAR A SENHA ======================================================================
+def bcrypt_hash(password:str):
+    # Encripta a senha
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=12))
     return hashed_password
 
+# VERIFICAR PASSWORD PARA LOGIN ===========================================================
 def verify_password(password:str, hashed_pass:bytes):
-    return bcrypt.checkpw(password.encode('utf-8'),hashed_pass)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_pass)
 
-def login_pass(username:str, password:str):
-    login_cadastrado = "admin"
-    senha_cadastrada = "admin123@"
+# FUNCAO PRECISA DE ALTERACOES ============================================================
+def login_pass():
+    print("LOGIN ====")
+    username = str(input("Usuario: "))
+    # CONDICAO DE EXISTENCIA USERNAME
+    cursor.execute("SELECT login from login_info WHERE login = ?", (username,))
+    resultado1 = cursor.fetchone() # COLETA INFORMAÇÃO DO BANCO
+    if resultado1 is None:
+        print("Usuário não encontrado")
+        return
+    else:
+        pass
 
-    if username == login_cadastrado and password == senha_cadastrada:
-        if verify_password(user_pass, hashed):
-            print("Senha verifica com sucesso!")
-        else:
-            print("Falha na verificação")
+    password = str(input("Senha: "))
+    # VERIFICAO DE SENHA HASH
+    cursor.execute("SELECT senha FROM login_info WHERE login = ?", (username,))
+    resultado2 = cursor.fetchone() # Pega a primeira linha encontrada
+
+    hash_do_banco = resultado2[0]
+    if bcrypt.checkpw(password.encode('utf-8'), hash_do_banco):
         print("Login efetuado com sucesso!")
+    else:
+        print("Senha incorreta!")
+
+
+# REGISTRAR USUARIO ========================================================================
+def register_user():
+    print("==== CADASTRO DE USUARIO ====")
+    username = str(input(("Usuario: ")))
+    password = str(input("Senha: "))
+
+    # Senha em hash (OBS:NUNCA SE GUARDA A SENHA ORIGINAL)
+    hashed = bcrypt_hash(password)
+
+    # SALVA NO BANCO
+    cursor.execute("""INSERT INTO login_info
+                    (login, senha) VALUES
+                    (?, ?) """, (username, hashed))
 
 # ==========================================================================================
 
-print("===== RANDOM WEBSITE LOGIN =====")
+print("="*30, " WEBSITE ", "="*30)
+'''
+print("[1] Login\n[2] Registrar-se\n[3] Sair")
 
-login = str(input("Login: "))
+user_option = int(input("Digite a opção desejada: "))
 
-user_pass = str(input("Senha: "))
+match user_option:
+    case 1:
+        
+    case 2:
+'''
 
-hashed = bcrypt_hash(user_pass)
+
+login_pass()
+#register_user()
+
+connection.commit()
+
+
+
+
+
+
 
 
 
